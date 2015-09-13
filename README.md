1a014510ee181d080f939c1c896c50581653e72a
black-lotus <donnydiunindra@gmail.com>
2015-09-13 19:51:35 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# android-game-kill-them-all-part-2
+Android Game - Kill Them All (2)
+================================
+
+part 1 : https://github.com/black-lotus/android-game-kill-them-all-part-1
+
+part 2 : https://github.com/black-lotus/android-game-kill-them-all-part-2
+
+
+A simple android game using surfaceview
+
+The game is tutorial from edu4java (http://www.edu4java.com/en/androidgame/androidgame1.html)
+
+I add some features, like :
+- random generator of bad and good guy
+- background, using drawPoint
+- and add scream audio, i download the audio from shockwave-sound (http://www.shockwave-sound.com/sound-effects/scream_sounds.html) 
+- custom bar, which has three members, those are total-angel, time and number-evil
+
+
+I am using Android Studio to develop this game
+
+Thanks, 
+
+Donny (https://github.com/black-lotus)
---
+++ app/src/main/java/com/donny/killthemall/BarView.java
@@ -0,0 +1,103 @@
+package com.donny.killthemall;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+/**
+ * Created by donny on 9/13/15.
+ */
+public class BarView {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Paint paint;
+    private int score = 0;
+    private int goodGuy = 0;
+    private int badGuy = 0;
+    private int time = 0;
+
+    public BarView(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        paint = new Paint();
+    }
+
+    public void onDraw(Canvas canvas) {
+        update();
+
+        int alpha = 255/2;
+        paint.setColor(Color.argb(alpha, 255, 255, 255));
+        RectF rf = new RectF(x, y, width, height);
+        canvas.drawRect(rf, paint);
+
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(42);
+        paint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(getTime(), width / 2, height - 42, paint);
+        paint.setTextAlign(Paint.Align.LEFT);
+        canvas.drawText("ANGLE : " + goodGuy, 10, height - 42, paint);
+        paint.setTextAlign(Paint.Align.RIGHT);
+        canvas.drawText("EVIL : " + badGuy, width-10, height - 42, paint);
+    }
+
+    private void update() {
+        time = ++time;
+    }
+
+    private String getTime() {
+        int tm = Math.round(time / GameView.FPS);
+        String tmMinute = "00";
+        String tmSecond = "00";
+        if (tm > 60) {
+            int tmpMinute = Math.round(tm / 60);
+            if (tmpMinute < 10) {
+                tmMinute = "0" + tmpMinute;
+            } else {
+                tmMinute = tmpMinute + "";
+            }
+
+            int tmpSecond = tm % 60;
+            if (tmpSecond < 10) {
+                tmSecond = "0" + tmpSecond;
+            } else {
+                tmSecond = tmpSecond + "";
+            }
+        } else {
+            if (tm < 10) {
+                tmSecond = "0" + tm;
+            } else {
+                tmSecond = tm + "";
+            }
+        }
+        return tmMinute +":"+ tmSecond;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getGoodGuy() {
+        return goodGuy;
+    }
+
+    public void setGoodGuy(int goodGuy) {
+        this.goodGuy = goodGuy;
+    }
+
+    public int getBadGuy() {
+        return badGuy;
+    }
+
+    public void setBadGuy(int badGuy) {
+        this.badGuy = badGuy;
+    }
+
+}
